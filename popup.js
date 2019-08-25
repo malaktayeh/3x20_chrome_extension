8b19b5a3f61c4a4b3a214534e44c6b3c008edd8f
Malak Tayeh <malaktayeh@outlook.com>
2019-08-25 19:38:38 -0400

    changed alarm and added function timeleft

--- popup.js
+++ popup.js
@@ -3,22 +3,35 @@
 // found in the LICENSE file.
 'use strict';
 
+let COUNTDOWN;
+
 function setAlarm(event) {
+  console.log('Set alarm!')
   let minutes = parseFloat(event.target.value);
   chrome.browserAction.setBadgeText({text: 'ON'});
-  chrome.alarms.create({delayInMinutes: minutes});
+  // add 20 min in milliseconds to current time
+  chrome.alarms.create(COUNTDOWN, {when: Date.now() + 1200000});
   chrome.storage.sync.set({minutes: minutes});
-  window.close();
 }
 
 function clearAlarm() {
   chrome.browserAction.setBadgeText({text: ''});
   chrome.alarms.clearAll();
-  window.close();
 }
 
+function timeLeft() {
+  chrome.alarms.get(COUNTDOWN, function(alarm) {
+    // get the leftover time
+    let timeLeft = alarm.scheduledTime - new Date().getTime();
+    let formattedTime = new Date(timeLeft).toISOString().slice(14, -5)
+    console.log(formattedTime)
+  })
+}
+
+if (window.open() === true) {
+  console.log('success!')
+}
 
 document.getElementById('sampleSecond').addEventListener('click', setAlarm);
-// document.getElementById('15min').addEventListener('click', setAlarm);
-// document.getElementById('30min').addEventListener('click', setAlarm);
 document.getElementById('cancelAlarm').addEventListener('click', clearAlarm);
+document.getElementById('timeLeft').addEventListener('click', timeLeft);
