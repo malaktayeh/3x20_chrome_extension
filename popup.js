8373b074b7b8bc8f9489db875960455e96117970
Malak Tayeh <malaktayeh@outlook.com>
2019-08-24 20:51:29 -0400

    refactored code and used chrome.alarms api for countdown from samples

--- popup.js
+++ popup.js
@@ -1,69 +1,24 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
 
-let totalSeconds = 5, // total seconds (25 minutes * 60 seconds)
-    minutes = 19,
-    seconds = 59,
-    stopCountDown = true,
-    countDown;
+function setAlarm(event) {
+  let minutes = parseFloat(event.target.value);
+  chrome.browserAction.setBadgeText({text: 'ON'});
+  chrome.alarms.create({delayInMinutes: minutes});
+  chrome.storage.sync.set({minutes: minutes});
+  window.close();
+}
 
-// Add event listeners once the DOM has fully loaded by listening for the
-// `DOMContentLoaded` event on the document, and adding your listeners to
-// specific elements when it triggers. 
-document.addEventListener('DOMContentLoaded', function() {
-    let start = document.getElementById('startButton'),
-        reset = document.getElementById('resetButton');
-        displayCountDown = document.getElementById('countDown');
-    start.addEventListener('click', controller);
-    reset.addEventListener('click', controller);
+function clearAlarm() {
+  chrome.browserAction.setBadgeText({text: ''});
+  chrome.alarms.clearAll();
+  window.close();
+}
 
-    function controller(e) {
-        console.log(e.target.innerHTML)
 
-        if (e.target.innerHTML === 'Start') {
-            start.innerHTML = "Pause";
-            !stopCountDown;
-            //start timer
-            countDownTimer = setInterval(
-                () => updateTimer(),
-                1000,
-            );
-        }
-        else if (e.target.innerHTML === 'Pause') {
-            start.innerHTML = "Start";
-            !stopCountDown;
-            //stop timer
-            clearTimeout(countDownTimer)
-        }
-        else if (e.target.innerHTML === 'Reset') {
-            resetCountDown();
-        }
-    }
-
-    function resetCountDown() {
-        start.innerHTML = "Start";
-        stopCountDown = true;
-        //stop timer and reset countDown
-        clearTimeout(countDownTimer);
-        totalSeconds = 1200;
-        minutes = 20;
-        seconds = 0;
-        displayCountDown.innerHTML = 20 + ':' + 00;
-    }
-
-    // Update the count down every 1 second
-    function updateTimer() {
-        console.log('in updateTimer function');
-
-        totalSeconds--;
-        minutes = parseInt(totalSeconds / 60);
-        seconds = parseInt(totalSeconds % 60);
-
-        // update display
-        displayCountDown.innerHTML = (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
-
-         // stops countdown if seconds and minutes are down to zero and resets variables
-         if (minutes === 0 && seconds === -1) {
-            resetCountDown();
-        }
-    } 
-
-})
+document.getElementById('sampleSecond').addEventListener('click', setAlarm);
+// document.getElementById('15min').addEventListener('click', setAlarm);
+// document.getElementById('30min').addEventListener('click', setAlarm);
+document.getElementById('cancelAlarm').addEventListener('click', clearAlarm);
