91d16c1d29ccbc5419ec56beef549a25aa1e7b3e
Malak Tayeh <malaktayeh@outlook.com>
2019-08-26 21:59:47 -0400

    failed: chrome.alarms api doesnt allow storage update every second as to current version :(

--- popup.js
+++ popup.js
@@ -3,7 +3,16 @@
 // found in the LICENSE file.
 'use strict';
 
+// variable for google chrome alarms apo
 let COUNTDOWN;
+// variable for the setTimeout function
+let countDownTimer;
+
+// bool value
+let isTimerOn;
+
+// gets countdown display
+let displayCountDown = document.getElementById('countDown');
 
 function setAlarm(event) {
   console.log('Set alarm!')
@@ -12,24 +21,44 @@ function setAlarm(event) {
   // add 20 min in milliseconds to current time
   chrome.alarms.create(COUNTDOWN, {when: Date.now() + 1200000});
   chrome.storage.sync.set({minutes: minutes});
+
+  // Save state using the Chrome extension storage API
+  chrome.storage.sync.set({'doesTimerExist': true}, function() {
+    // Notify that we saved.
+    console.log('Set doesTimerExist to true in chrome storage');
+  });
+  displayTime();
 }
 
 function clearAlarm() {
-  chrome.browserAction.setBadgeText({text: ''});
+  chrome.browserAction.setBadgeText({text: 'OFF'});
   chrome.alarms.clearAll();
+  console.log('Cleared timer!');
+  clearTimeout(countDownTimer);
+  displayCountDown.innerHTML = '';
+}
+
+function displayTime() {
+  chrome.storage.sync.get(['doesTimerExist'], function(result) {
+    // Notify the return value
+    if (result.doesTimerExist === true) timeLeft();
+  });
+
 }
 
 function timeLeft() {
-  chrome.alarms.get(COUNTDOWN, function(alarm) {
-    // get the leftover time
-    let timeLeft = alarm.scheduledTime - new Date().getTime();
-    let formattedTime = new Date(timeLeft).toISOString().slice(14, -5)
-    console.log(formattedTime)
-  })
-}
+  let timeLeftTemp, formattedTime;
 
-if (window.open() === true) {
-  console.log('success!')
+  chrome.alarms.get(COUNTDOWN, function(alarm) {
+    if (alarm !== undefined) {
+      timeLeftTemp = alarm.scheduledTime - new Date().getTime();
+      formattedTime = new Date(timeLeftTemp).toISOString().slice(14, -5);
+  
+      console.log(formattedTime);
+      displayCountDown.innerHTML = formattedTime;
+    }
+    else displayCountDown.innerHTML = 'Did you forget to turn the reminder on?';
+  });
 }
 
 document.getElementById('sampleSecond').addEventListener('click', setAlarm);
